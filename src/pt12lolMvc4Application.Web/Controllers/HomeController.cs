21563e9f0507acc1388f46485728dc4c8111f18b
pt12lol <pt12lol@gmail.com>
2014-04-07 23:47:20 +0200

    LogHelper added to HomeController

--- src/pt12lolMvc4Application.Web/Controllers/HomeController.cs
+++ src/pt12lolMvc4Application.Web/Controllers/HomeController.cs
@@ -3,48 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 using log4net;
+using pt12lolMvc4Application.Services;
+using pt12lolMvc4Application.Services.Interfaces;
 
 namespace pt12lolMvc4Application.Web.Controllers
 {
     public class HomeController : Controller
     {
-        protected readonly ILog log;
+        readonly ILog log;
+        readonly ILogHelper logHelper;
 
         public HomeController()
         {
             log = LogManager.GetLogger(this.GetType());
-            log.Debug("Should not be seen.");
-            log.Info("Hello, world!");
-            try
-            {
-                throw new Exception();
-            }
-            catch
-            {
-                log.Error("Exception caught!");
-            }
+            logHelper = new LogHelper();
         }
 
         public ActionResult Index()
         {
+            log.Info(logHelper.FormatLog(Request));
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
         public ActionResult About()
         {
+            log.Info(logHelper.FormatLog(Request));
             ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
+            log.Info(logHelper.FormatLog(Request));
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
