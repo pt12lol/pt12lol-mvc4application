8c1d45e94625773f127c651d9842b6bfaee77918
pt12lol <pt12lol@gmail.com>
2014-04-12 00:28:32 +0200

    UsersController with admin's authorization required added

---
+++ src/pt12lolMvc4Application.Web/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace pt12lolMvc4Application.Web.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
